85c8bab1005cf299d891175d11bfc727ca7a2d8c
phil-mb <ph.ludewig@gmail.com>
2019-07-03 22:13:14 +0200

    Remove Other Jetson Pinconfigs

--- GPIO_Tests/jetsonTX2GPIO/jetsonGPIO.h
+++ GPIO_Tests/jetsonTX2GPIO/jetsonGPIO.h
@@ -74,6 +74,7 @@ enum jetsonGPIONumber {
 	gpio166 = 166     // J3A2 - Pin 58
 }  ;
 
+/*
 enum jetsonTX1GPIONumber {
        gpio36 = 36,      // J21 - Pin 32 - Unused - AO_DMIC_IN_CLK
        gpio37 = 37,      // J21 - Pin 16 - Unused - AO_DMIC_IN_DAT
@@ -84,6 +85,8 @@ enum jetsonTX1GPIONumber {
        gpio187 = 187,    // J21 - Pin 37 - Output - GPIO8_ALS_PROX_INT
        gpio219 = 219,    // J21 - Pin 29 - Output - GPIO19_AUD_RST
 } ;
+
+
 
 enum jetsonTX2GPIONumber {
        gpio398 = 398,      // J21 - Pin 29 - GPIO5
@@ -96,6 +99,8 @@ enum jetsonTX2GPIONumber {
        gpio394 = 394,      // J21 - Pin 38 - GPIO20
        gpio393 = 393,      // J21 - Pin 40 - GPIO21
 } ;
+
+*/
 
 enum jetsonNanoGPIONumber {
        gpio216 = 216,      // J21 - Pin 29 - GPIO5
