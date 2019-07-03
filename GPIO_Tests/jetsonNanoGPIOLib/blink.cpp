2c3ca8adbaa99197495cfb9c9d219d777694e10b
phil-mb <ph.ludewig@gmail.com>
2019-07-03 22:52:25 +0200

    Update blink.cpp

--- GPIO_Tests/jetsonNanoGPIOLib/blink.cpp
+++ GPIO_Tests/jetsonNanoGPIOLib/blink.cpp
@@ -18,7 +18,7 @@ int getkey() {
     struct termios orig_term_attr;
     struct termios new_term_attr;
 
-    /* set the terminal to raw mode
+    // set the terminal to raw mode
     tcgetattr(fileno(stdin), &orig_term_attr);
     memcpy(&new_term_attr, &orig_term_attr, sizeof(struct termios));
     new_term_attr.c_lflag &= ~(ECHO|ICANON);
@@ -26,11 +26,11 @@ int getkey() {
     new_term_attr.c_cc[VMIN] = 0;
     tcsetattr(fileno(stdin), TCSANOW, &new_term_attr);
 
-    /* read a character from the stdin stream without blocking
-    /*   returns EOF (-1) if no character is available
+    // read a character from the stdin stream without blocking
+    //   returns EOF (-1) if no character is available
     character = fgetc(stdin);
 
-    /* restore the original terminal attributes
+    // restore the original terminal attributes
     tcsetattr(fileno(stdin), TCSANOW, &orig_term_attr);
 
     return character;
